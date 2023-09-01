c6841ea54888e057216f608b5cf5cf5e25efef43
Yuvraj518 <124797389+Yuvraj518@users.noreply.github.com>
2023-09-01 09:37:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const p1=new Promise((resolve,reject)=>{
 	setTimeout(()=>{resolve("a")},3000)
 });
 const p2=new Promise((resolve,reject)=>{
-	setTimeout(()=>{resolve()},1000)
+	setTimeout(()=>{resolve("b")},1000)
 })
 const p3=new Promise((resolve,reject)=>{
-	setTimeout(()=>{resolve()},3000)
+	setTimeout(()=>{resolve("c")},3000)
 })
 console.log(p1,p2,p3)
 promises=[p1,p2,p3];
