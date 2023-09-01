92ff70e74c8871cf92f418f6684246ab2ce09323
Yuvraj518 <124797389+Yuvraj518@users.noreply.github.com>
2023-09-01 09:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-window.promises = [];
-
-// Do not change the code above this
-// add your promises to the array `promises`
-const p1=new Promise(resolve,reject)=>{
-	set.Timeout(()=>{resolve()},3000)
-}
-const p2=new Promise(resolve,reject)=>{
-	set.Timeout(()=>{resolve()},1000)
-}
-const p3=new Promise(resolve,reject)=>{
-	set.Timeout(()=>{resolve()},3000)
-}
+const p1=new Promise((resolve,reject)=>{
+	setTimeout(()=>{resolve("a")},3000)
+});
+const p2=new Promise((resolve,reject)=>{
+	setTimeout(()=>{resolve()},1000)
+})
+const p3=new Promise((resolve,reject)=>{
+	setTimeout(()=>{resolve()},3000)
+})
+console.log(p1,p2,p3)
 promises=[p1,p2,p3];
-Promise.any(promises).then((x)=>{console,log(x)})
+Promise.any(promises).then((x)=>{console.log(x)})
